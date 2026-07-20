d87e862da5733bcb33bacf2563c147d50111d7fb
ricse <anthony.ricse98@gmail.com>
2026-07-20 11:47:52 -0500

    feat(*): updateprofile pages

---
+++ app/sitemap.ts
@@ -0,0 +1,60 @@
+export default async function sitemap() {
+  // Aquí puedes hacer fetch de tu base de datos o API si tienes URLs dinámicas
+  // const posts = await getPosts(); 
+  
+  return [
+    {
+      url: 'https://comerce-electronic-app.vercel.app/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://comerce-electronic-app.vercel.app/electronic-group/maintenance',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://comerce-electronic-app.vercel.app/rent',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://comerce-electronic-app.vercel.app/sell',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://comerce-electronic-app.vercel.app/replacement',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://comerce-electronic-app.vercel.app/electronic-board',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://comerce-electronic-app.vercel.app/fotovoltaic-system',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },{
+      url: 'https://comerce-electronic-app.vercel.app/fotovoltaic-system',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://comerce-electronic-app.vercel.app/electromovil',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    }
+  ]
+}
